cc5b08f8add87c4cfddeb81bea7d328f70a19426
KILLCOMKIDSS <146170593+KILLCOMKIDSS@users.noreply.github.com>
2024-08-18 04:43:26 -0700

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,1 @@
-import sqlite3
-
-def initialize_db():
-    conn = sqlite3.connect('db.db')  # Adjust the path as needed
-    with open('schema.sql', 'r') as f:
-        conn.executescript(f.read())
-    conn.close()
-
-if __name__ == '__main__':
-    initialize_db()
+INITIALIZER FOR DATABASE N/A just forgot to delete lol
